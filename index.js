ce43494991a5a183ea05b58d9d3ab5b58d6640e2
Server OBI <brian.rivaldy@binus.ac.id>
2022-12-12 16:56:27 +0700

    tambah setting db koneksi

--- index.js
+++ index.js
@@ -6,18 +6,28 @@ const port = process.env.PORT || 3500;
 
 let db = require("./config/db");
 
-const connect = async () => {
+//const connect = async () => {
 
-try{
-    let sql  = await db();
-    const result = await sql.query`select * from dbo.USR50;`;
-    console.log("Jumlah data : ", result.recordset.length);
-    
-    }catch(err) {
-        console.log("err ", err);
-    }
-}
+//try{
+//    let sql  = await db();
+//  const result = await sql.query`select * from dbo.USR50;`;
+//    console.log("Jumlah data : ", result.recordset.length);
+//   
+//    }catch(err) {
+//        console.log("err ", err);
+//    }
+//}
 //connect();
+
+const sql = require("msnodesqlv8");
+
+const connectionString = "server=WIN-1G5BO4FRUM4;Database=OBI_LIVE;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
+const query = "SELECT * from dbo.WTQ1";
+
+sql.query(connectionString, query, (err, rows) => {
+    console.log(rows);
+    console.log("tugas kuliah: ", err);
+});
 
 app.use(express.static(path.resolve(__dirname, './build')));
 
