773b84fba297d3c5d4a6cca60b8cc31971e188c5
komarudin <help@tell.co.id>
2023-02-19 12:20:39 +0700

    update

--- config/mongo_db.js
+++ config/mongo_db.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/OBI');
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  try{
+    var koneksi = await mongoose.connect('mongodb://'+process.env.DB_HOST+'/'+process.env.DB_NAME);
+    console.log("================");
+    console.log("koneksi berhasil");
+    console.log("================");
+    return "koneksi aman";
+  }catch(e){
+    console.log("e ", e);
+    return e.message;
+  }
 }
