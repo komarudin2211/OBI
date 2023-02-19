7d3497cf8b008b6be749cdeaef8caa7b663d8254
komarudin <help@tell.co.id>
2023-02-19 12:35:41 +0700

    update

--- config/mongo_db.js
+++ config/mongo_db.js
@@ -5,7 +5,13 @@ main().catch(err => console.log(err));
 
 async function main() {
   try{
-    var koneksi = await mongoose.connect('mongodb://'+process.env.DB_HOST+'/'+process.env.DB_NAME);
+    if(process.env.NODE_ENV == 'PRODUCTION'){
+      await mongoose.connect(process.env.DB_HOST);
+    }
+    else{
+      await mongoose.connect('mongodb://'+process.env.DB_HOST+'/'+process.env.DB_NAME);
+    }
+
     console.log("================");
     console.log("koneksi berhasil");
     console.log("================");
